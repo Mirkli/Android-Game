ee82b08d9936db2c93c3c8b0851f09fede4d0724
test <test@mail.ru>
2016-01-14 21:49:56 +0200

    Platvormide kontroll

---
+++ Zone.java
@@ -0,0 +1,16 @@
+package com.example.user.magnetgame;
+
+/**
+ * Created by User on 02.01.2016.
+ */
+public class Zone {
+    double X1,X2,Y2,Y1;
+
+    public Zone(double x1,double x2,double y1,double y2){
+        X1=x1;
+        X2=x2;
+        Y2=y2;
+        Y1=y1;
+    }
+}
+
